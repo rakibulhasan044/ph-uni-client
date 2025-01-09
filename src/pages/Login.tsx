2ad21610eed3de65198258b70f052a325ea0f233
rakibulhasan044 <rhrakib044@gmail.com>
2025-01-10 03:16:51 +0600

    store user in the redux store

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,53 @@
-import React from 'react';
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/feature/auth/authApi";
+import { useAppDispatch } from "../redux/hook";
+import { setUser } from "../redux/feature/auth/authSlice";
+import { verifyToken } from "../utils/verifyToken";
+
+type TLoginData = {
+    id: string;
+    password: string
+}
 
 const Login = () => {
-    return (
+
+    const dispatch = useAppDispatch()
+    const { register, handleSubmit } = useForm({
+        defaultValues: {
+            id: 'A-0001',
+            password: 'admin123'
+        }
+    });
+
+    const [login, {error}] = useLoginMutation();
+    console.log("error=>", error);
+
+    const onSubmit = async (data: TLoginData) => {
+        const userInfo = {
+            id: data.id,
+            password: data.password
+        }
+        const res = await login(userInfo).unwrap();
+        const user = verifyToken(res.data.accessToken)
+        console.log(user);
+        dispatch(setUser({ user: user, token: res.data.accessToken}))
+    }
+  return (
+    <div style={{ backgroundColor: "gray", height: "100vh" }}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div>
-            <h1>this is login page</h1>
+          <label htmlFor="id">ID:</label>
+          <input type="text" id="id" {...register('id')}/>
         </div>
-    );
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input type="text" id="password" {...register('password')}/>
+        </div>
+        <Button htmlType="submit">Login</Button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
