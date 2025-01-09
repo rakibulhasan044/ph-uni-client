cfbecd19c9304ecb5dd19a233a6e5526046b6aef
rakibulhasan044 <rhrakib044@gmail.com>
2025-01-10 02:18:45 +0600

    added auth store and base api

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./feature/auth/authSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    auth: authReducer,
+  },
 });
 
-
 // Infer the `RootState`,  `AppDispatch`, and `AppStore` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
-export type AppStore = typeof store
+export type AppDispatch = typeof store.dispatch;
+export type AppStore = typeof store;
