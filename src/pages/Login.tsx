8e8ebd11e7222436697aec95b74bb8e0b0175052
rakibulhasan044 <rhrakib044@gmail.com>
2025-01-10 03:29:09 +0600

    set user in the local storage

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -6,43 +6,42 @@ import { setUser } from "../redux/feature/auth/authSlice";
 import { verifyToken } from "../utils/verifyToken";
 
 type TLoginData = {
-    id: string;
-    password: string
-}
+  id: string;
+  password: string;
+};
 
 const Login = () => {
+  const dispatch = useAppDispatch();
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      id: "A-0001",
+      password: "admin123",
+    },
+  });
 
-    const dispatch = useAppDispatch()
-    const { register, handleSubmit } = useForm({
-        defaultValues: {
-            id: 'A-0001',
-            password: 'admin123'
-        }
-    });
+  const [login, { error }] = useLoginMutation();
+  console.log("error=>", error);
 
-    const [login, {error}] = useLoginMutation();
-    console.log("error=>", error);
+  const onSubmit = async (data: TLoginData) => {
+    const userInfo = {
+      id: data.id,
+      password: data.password,
+    };
+    const res = await login(userInfo).unwrap();
+    const user = verifyToken(res.data.accessToken);
+    dispatch(setUser({ user: user, token: res.data.accessToken }));
+  };
 
-    const onSubmit = async (data: TLoginData) => {
-        const userInfo = {
-            id: data.id,
-            password: data.password
-        }
-        const res = await login(userInfo).unwrap();
-        const user = verifyToken(res.data.accessToken)
-        console.log(user);
-        dispatch(setUser({ user: user, token: res.data.accessToken}))
-    }
   return (
     <div style={{ backgroundColor: "gray", height: "100vh" }}>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div>
           <label htmlFor="id">ID:</label>
-          <input type="text" id="id" {...register('id')}/>
+          <input type="text" id="id" {...register("id")} />
         </div>
         <div>
           <label htmlFor="password">Password:</label>
-          <input type="text" id="password" {...register('password')}/>
+          <input type="text" id="password" {...register("password")} />
         </div>
         <Button htmlType="submit">Login</Button>
       </form>
