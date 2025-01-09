8341228320c26d103e40379f437636b83aad251c
rakibulhasan044 <rhrakib044@gmail.com>
2025-01-09 23:18:17 +0600

    basic redux setup

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -4,11 +4,7 @@ import About from "../pages/About";
 import Login from "../pages/Login";
 import Contact from "../pages/Contact";
 import Register from "../pages/Register";
-import AdminDashboard from "../pages/admin/AdminDashboard";
-import CreateAdmin from "../pages/admin/CreateAdmin";
-import CreateStudent from "../pages/admin/CreateStudent";
-import CreateFaculty from "../pages/admin/CreateFaculty";
-import { adminPaths } from "./admin.routes";
+import { adminRoutes } from "./admin.routes";
 
 const router = createBrowserRouter([
   {
@@ -28,19 +24,19 @@ const router = createBrowserRouter([
   {
     path: "/admin",
     element: <App />,
-    children: adminPaths,
+    children: adminRoutes,
   },
 
   {
     path: "/faculty",
     element: <App />,
-    children: adminPaths,
+    children: adminRoutes,
   },
 
   {
     path: "/student",
     element: <App />,
-    children: adminPaths,
+    children: adminRoutes,
   },
 
   {
